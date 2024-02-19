c7faf2c51a048b954717df2ec4e3aca11ec4d8c9
pabcrudel <pabcrudel@gmail.com>
2024-02-19 19:27:27 +0100

    feat: Display basic metadata tags

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,11 @@
 import { plexSans, plexSerif } from '../fonts/ibm-plex';
 import '../style/main.css';
 
+export const metadata = {
+  title: 'Next.js + Prisma + Vercel Postgres',
+  description: 'Fullstack App with Next.js, Prisma, and Vercel Postgres'
+};
+
 export default function RootLayout ({ children }) {
   return (
     <html lang="en" className={`${plexSans.variable} ${plexSerif.variable}`}>
