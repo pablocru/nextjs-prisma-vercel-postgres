400777f3e8a64e3611135c1bc8a5b842faac2206
pabcrudel <pabcrudel@gmail.com>
2024-02-20 13:04:14 +0100

    feat: Wrap each section using semantic html tags

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,9 +3,14 @@ import { Feed } from '../components/feed';
 export default function Page () {
   return (
     <>
-      <h1>Next.js + Prisma + Vercel Postgres</h1>
-      <p>Fullstack App with Next.js, Prisma, and Vercel Postgres</p>
-      <Feed/>
+      <header>
+        <h1>Next.js + Prisma + Vercel Postgres</h1>
+        <p>Fullstack App with Next.js, Prisma, and Vercel Postgres</p>
+      </header>
+
+      <main>
+        <Feed/>
+      </main>
     </>
   );
 }
