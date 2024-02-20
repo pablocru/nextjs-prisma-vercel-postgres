9a31f0beb23f92342a2d700f805102889f784645
pabcrudel <pabcrudel@gmail.com>
2024-02-20 17:27:14 +0100

    feat: Create a page to dynamically display posts

---
+++ src/app/[id]/page.jsx
@@ -0,0 +1,26 @@
+import ReactMarkdown from 'react-markdown';
+import { getPostByID, getValidIDs } from '../../lib/prisma/post';
+
+export const dynamicParams = false;
+
+export async function generateStaticParams () {
+  const posts = await getValidIDs();
+
+  return posts.map(post => ({
+    id: post.id
+  }));
+}
+
+export default async function Post ({ params }) {
+  const post = await getPostByID(params.id);
+
+  return (
+    <main>
+      <h1>{post.title}</h1>
+      <ReactMarkdown>
+        {post.content}
+      </ReactMarkdown>
+      <p>By {post.author.name}</p>
+    </main>
+  );
+}
