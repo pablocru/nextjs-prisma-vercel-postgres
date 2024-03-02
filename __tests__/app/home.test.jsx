c6fe7086c83bd26412c38a34ea055d52e671fb5c
pabcrudel <pabcrudel@gmail.com>
2024-03-02 19:21:47 +0100

    test: Render feed post or disclaimer depending on database response

--- __tests__/app/home.test.jsx
+++ __tests__/app/home.test.jsx
@@ -1,13 +1,16 @@
-import { expect, describe, afterEach, it } from 'vitest';
+import { expect, describe, afterEach, it, vi } from 'vitest';
 import { render, screen, cleanup } from '@testing-library/react';
 import Home from '@/app/page';
+import { fetchFeed } from '@/prisma/post';
+import posts from '../mocks/posts.json';
+
+vi.mock('@/prisma/post', () => ({
+  fetchFeed: vi.fn()
+}));
+fetchFeed.mockReturnValue([]);
 
 describe('Home page', () => {
   afterEach(cleanup);
-
-  it('should render', async () => {
-    render(await Home());
-  });
 
   it('should render a level 1 heading', async () => {
     render(await Home());
@@ -16,6 +19,32 @@ describe('Home page', () => {
       screen.getByRole(
         'heading',
         { level: 1, name: 'Next.js + Prisma + Vercel Postgres' }
+      )
+    ).toBeDefined();
+  });
+
+  it('should render feed posts', async () => {
+    fetchFeed.mockReturnValueOnce(posts);
+
+    render(await Home());
+
+    for (const post of posts) {
+      expect(
+        screen.getByRole(
+          'heading',
+          { level: 3, name: post.title }
+        )
+      ).toBeDefined();
+    }
+  });
+
+  it('should render a disclaimer if there is any post yet', async () => {
+    render(await Home());
+
+    expect(
+      screen.getByRole(
+        'heading',
+        { level: 3, name: 'There isn\'t any post yet' }
       )
     ).toBeDefined();
   });
